9c10ebeb7e1d3448df70dcb02e04174062292e1a
Sabbir-hossain1 <sabbir.cse.duet@gmail.com>
2022-06-22 02:30:11 +0600

    Blind auction program which will find the winner of the auctionblindly

---
+++ blind Auction.py
@@ -0,0 +1,31 @@
+import os
+bidding_dictionary = {}
+bidding_finish = False
+
+
+def find_height_bidder(bidding_record):
+    highest_bidding_amount=0
+    winner = ""
+    
+    for bidder in bidding_record:
+        bidding_amount = bidding_record[bidder]
+        if(bidding_amount > highest_bidding_amount):
+            highest_bidding_amount = bidding_amount
+            winner = bidder
+            
+    print(f"Thw winner is {winner} with a bid of ${highest_bidding_amount}")
+
+while not bidding_finish:
+    name = input("What is your name? ")
+    bid_price = int(input("your bidding price ? "))
+    bidding_dictionary[name] = bid_price
+    should_continue = input("Are there any bidders? Type 'yes' or 'no' ")
+    if should_continue == "no":
+        bidding_finish = True
+        find_height_bidder(bidding_dictionary)
+    elif should_continue == "yes":
+        os.system('cls')
+        
+        
+
+
